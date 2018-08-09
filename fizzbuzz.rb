b770d88c5c1370bf15b911656a3495857bbc2e17
Adam Duff <j.adam.duff@gmail.com>
2018-08-09 05:57:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if (num / 3).to_i.is_a? Integer
+  if ((num / 3).to_i.is_a? Integer) && ((num / 5).to_i.is_a? Integer)
+    return "FizzBuzz"
+  elsif (num / 3).to_i.is_a? Integer
     return "Fizz"
-  elsif (num / 5).to_i.is_a? Integer
+  elsif (num / 3).to_i.is_a? Integer
     return "Buzz"
   else
     return nil
