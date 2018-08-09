b3c881087f3ae8c4db6bb7407fc606cc089a4ded
Adam Duff <j.adam.duff@gmail.com>
2018-08-09 05:52:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  if ((num / 3).to_i.is_a? Integer) && ((num / 5).to_i.is_a? Integer)
+    return "FizzBuzz"
+  elsif (num / 3).to_i.is_a? Integer
+    return "Fizz"
+  elsif (num / 3).to_i.is_a? Integer
+    return "Buzz"
+  end
+end
